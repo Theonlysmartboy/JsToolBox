7f27a3aa27f3d3b9c92af4e3b5d21ddd22e9b0a7
Theonlysmartboy <o2jose43@gmail.com>
2026-01-14 15:54:18 +0300

    Add project files.

---
+++ JsToolBox/Base/LoaderBase.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+
+Public MustInherit Class LoaderBase
+    Inherits Control
+
+    Private _timer As Timer
+
+    Public Property LoaderColor As Color = Color.DodgerBlue
+    Public Property Speed As Integer = 100 ' ms interval
+
+    Public Sub New()
+        Me.DoubleBuffered = True
+        _timer = New Timer()
+        _timer.Interval = Speed
+        AddHandler _timer.Tick, AddressOf OnTick
+    End Sub
+
+    Public Sub Start()
+        _timer.Start()
+    End Sub
+
+    Public Sub [Stop]()
+        _timer.Stop()
+        Me.Invalidate()
+    End Sub
+
+    Protected MustOverride Sub OnTick(sender As Object, e As EventArgs)
+End Class
---
+++ JsToolBox/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("JsToolBox")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("JsToolBox")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b4bc3f68-7dc7-46bc-90ba-8e9b9e790d34")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
