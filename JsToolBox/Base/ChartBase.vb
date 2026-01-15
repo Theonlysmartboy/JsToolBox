c2b0592981130621b06ca7a309d32e642b9d3444
Theonlysmartboy <o2jose43@gmail.com>
2026-01-15 17:46:19 +0300

    Added Charts

--- JsToolBox/Base/ChartBase.vb
+++ JsToolBox/Base/ChartBase.vb
@@ -2,95 +2,73 @@
 Imports System.Drawing
 Imports System.Windows.Forms
 Imports System.Windows.Forms.DataVisualization.Charting
-
 Namespace Base
-
     Public MustInherit Class ChartBase
         Inherits UserControl
-
         Protected ReadOnly _chart As Chart
         Private _hoveredPoint As DataPoint = Nothing
         Private _originalColor As Color
         Private _targetMarkerSize As Integer = 0
         Private _animationTimer As Timer
-        Private _glowIncrement As Integer = 1 ' How fast the grow/shrink happens
+        Private _glowIncrement As Integer = 1 ' How fast the grow/shrink happens 
         Private _maxGlowSize As Integer = 12
-
         Public Sub New()
             Me.DoubleBuffered = True
             Me.Size = New Size(400, 300)
-
-            ' Initialize chart
+            ' Initialize chart 
             _chart = New Chart() With {.Dock = DockStyle.Fill}
             Me.Controls.Add(_chart)
-
             Dim area As New ChartArea("MainArea")
             _chart.ChartAreas.Add(area)
-
-            Dim series As New Series("Series1")
-            _chart.Series.Add(series)
-
             Dim legend As New Legend("Legend1")
             _chart.Legends.Add(legend)
-
-            ' Hover detection
+            ' Default series 
+            Dim s As New Series("Series1")
+            _chart.Series.Add(s)
+            ' Hover detection 
             AddHandler _chart.MouseMove, AddressOf Chart_MouseMove
             AddHandler _chart.MouseLeave, AddressOf Chart_MouseLeave
-
-            ' Timer for smooth animation
-            _animationTimer = New Timer With {.Interval = 15} ' ~60 FPS
+            ' Timer for smooth animation 
+            _animationTimer = New Timer With {.Interval = 15} ' ~60 FPS 
             AddHandler _animationTimer.Tick, AddressOf AnimateHoveredPoint
             _animationTimer.Start()
         End Sub
-
-        ' Hover detection
+        ' Hover detection 
         Private Sub Chart_MouseMove(sender As Object, e As MouseEventArgs)
             Dim result = _chart.HitTest(e.X, e.Y)
             Dim point As DataPoint = Nothing
-
             If result.ChartElementType = ChartElementType.DataPoint Then
                 point = _chart.Series(result.Series.Name).Points(result.PointIndex)
             End If
-
             If point IsNot _hoveredPoint Then
                 ResetPreviousPoint()
                 _hoveredPoint = point
                 If _hoveredPoint IsNot Nothing Then
-                    _originalColor = _hoveredPoint.Color
-                    _targetMarkerSize = _hoveredPoint.MarkerSize + _maxGlowSize
-                    _hoveredPoint.MarkerStyle = MarkerStyle.Circle
+                    _originalColor = If(_hoveredPoint.Color.IsEmpty, _hoveredPoint.BackSecondaryColor, _hoveredPoint.Color)
                 End If
             End If
         End Sub
-
+        Private Sub AnimateHoveredPoint(sender As Object, e As EventArgs)
+            If _hoveredPoint IsNot Nothing Then
+                ' Apply glow effect using a lightened color 
+                _hoveredPoint.Color = ControlPaint.Light(_originalColor, 0.5F)
+                ' Optionally, add border 
+                _hoveredPoint.BorderColor = _originalColor
+                _hoveredPoint.BorderWidth = 2
+            End If
+        End Sub
         Private Sub Chart_MouseLeave(sender As Object, e As EventArgs)
             ResetPreviousPoint()
         End Sub
-
-        ' Smooth animation per tick
-        Private Sub AnimateHoveredPoint(sender As Object, e As EventArgs)
-            If _hoveredPoint IsNot Nothing Then
-                ' Grow towards target size
-                If _hoveredPoint.MarkerSize < _targetMarkerSize Then
-                    _hoveredPoint.MarkerSize += _glowIncrement
-                    _hoveredPoint.Color = ControlPaint.Light(_originalColor, 0.5F)
-                End If
-            End If
-        End Sub
-
         Private Sub ResetPreviousPoint()
             If _hoveredPoint IsNot Nothing Then
-                ' Shrink back to original size
-                _hoveredPoint.MarkerSize -= _glowIncrement
-                If _hoveredPoint.MarkerSize <= 5 Then
-                    _hoveredPoint.MarkerStyle = MarkerStyle.None
-                    _hoveredPoint.Color = _originalColor
-                    _hoveredPoint = Nothing
-                End If
+                ' Restore original color 
+                _hoveredPoint.Color = _originalColor
+                _hoveredPoint.BorderWidth = 1
+                _hoveredPoint = Nothing
             End If
         End Sub
-
-        ' Designer Properties
+        ' Designer Properties 
         <Browsable(True), Category("Appearance"), DefaultValue("Chart Title")>
         Public Property Title As String
             Get
@@ -102,7 +80,6 @@ Namespace Base
                 _chart.Titles.Add(value)
             End Set
         End Property
-
         <Browsable(True), Category("Appearance"), DefaultValue(ChartColorPalette.Bright)>
         Public Property Palette As ChartColorPalette
             Get
@@ -112,7 +89,6 @@ Namespace Base
                 _chart.Palette = value
             End Set
         End Property
-
         <Browsable(True), Category("Appearance"), DefaultValue(True)>
         Public Property ShowLegend As Boolean
             Get
@@ -122,7 +98,6 @@ Namespace Base
                 _chart.Legends(0).Enabled = value
             End Set
         End Property
-
         <Browsable(True), Category("Axes"), DefaultValue("")>
         Public Property AxisXTitle As String
             Get
@@ -132,7 +107,6 @@ Namespace Base
                 _chart.ChartAreas(0).AxisX.Title = value
             End Set
         End Property
-
         <Browsable(True), Category("Axes"), DefaultValue("")>
         Public Property AxisYTitle As String
             Get
@@ -142,7 +116,6 @@ Namespace Base
                 _chart.ChartAreas(0).AxisY.Title = value
             End Set
         End Property
-
         <Browsable(True), Category("Data"), DefaultValue("Series1")>
         Public Property SeriesName As String
             Get
@@ -157,20 +130,19 @@ Namespace Base
                 End If
             End Set
         End Property
-
+        Private _chartType As SeriesChartType = SeriesChartType.Column
         <Browsable(True), Category("Appearance")>
         Public Property ChartType As SeriesChartType
             Get
-                If _chart.Series.Count = 0 Then Return SeriesChartType.Column
-                Return _chart.Series(0).ChartType
+                Return _chartType
             End Get
             Set(value As SeriesChartType)
+                _chartType = value
                 For Each s As Series In _chart.Series
                     s.ChartType = value
                 Next
             End Set
         End Property
-
         <Browsable(False)>
         Public WriteOnly Property DataSource As DataTable
             Set(value As DataTable)
@@ -184,19 +156,23 @@ Namespace Base
                 Next
             End Set
         End Property
-
         <Browsable(False)>
         Public Sub SetData(xValues() As String, yValues() As Double)
-            If xValues.Length <> yValues.Length Then Throw New ArgumentException("X and Y arrays must match length")
+            If xValues.Length <> yValues.Length Then
+                Throw New ArgumentException("X and Y arrays must match length")
+            End If
             _chart.Series.Clear()
             Dim series As New Series(SeriesName) With {.ChartType = Me.ChartType}
-            For i As Integer = 0 To xValues.Length - 1
-                series.Points.AddXY(xValues(i), yValues(i))
-            Next
             _chart.Series.Add(series)
+            For i As Integer = 0 To xValues.Length - 1
+                Dim p = series.Points.AddXY(xValues(i), yValues(i))
+                series.Points(p).Color = GetColor(i)
+            Next
         End Sub
-
+        Private Function GetColor(index As Integer) As Color
+            Dim colors As Color() = {Color.FromArgb(255, 99, 132), Color.FromArgb(54, 162, 235), Color.FromArgb(255, 206, 86), Color.FromArgb(75, 192, 192), Color.FromArgb(153, 102, 255), Color.FromArgb(255, 159, 64)}
+            Return colors(index Mod colors.Length)
+        End Function
         Protected MustOverride Sub OnTick(sender As Object, e As EventArgs)
-
     End Class
 End Namespace
